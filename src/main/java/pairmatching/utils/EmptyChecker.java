ebcbce568f8ae64946bd2f4956fc7d358883ca1b
choiejae <ejae8319@gmail.com>
2022-12-17 00:15:03 +0900

    feat: 입력값이 Empty 한지 확인하는 EmptyChecker 생성

---
+++ src/main/java/pairmatching/utils/EmptyChecker.java
@@ -0,0 +1,16 @@
+package pairmatching.utils;
+
+public class EmptyChecker {
+    private EmptyChecker() {
+    }
+
+    public static void check(final String input) {
+        if (isEmptyOrNull(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isEmptyOrNull(String str) {
+        return str == null || str.isEmpty();
+    }
+}
